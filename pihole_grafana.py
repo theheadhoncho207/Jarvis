b41237a4a71615e5f5f2832c11a7d0a0b08331ed
theheadhoncho207 <benmorse@meraki.com>
2017-01-01 17:27:31 -0800

    Update pihole_grafana.py

--- pihole_grafana.py
+++ pihole_grafana.py
@@ -1,10 +1,6 @@
 #! /usr/bin/python
 
-#Script originally created by JON HAYWARD: https://fattylewis.com/Graphing-pi-hole-stats/
-#Adapted fo work with InfluxDB by /u/tollsjo in December 2016
-
 # To install and run the script as a service under SystemD. See: https://linuxconfig.org/how-to-automatically-execute-shell-script-at-startup-boot-on-systemd-linux
-
 
 import requests
 import time
